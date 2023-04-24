9c7e030870fd9b1b4e412baa61a7496cd08a018f
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2023-04-24 22:26:41 +0200

    Stub more registers

--- src/core/cpu/cp15.cpp
+++ src/core/cpu/cp15.cpp
@@ -12,10 +12,16 @@ namespace nds::cpu::cp15 {
 
 enum CP15Reg {
     Control  = 0x0100,
+    CDPR     = 0x0200,
+    CIPR     = 0x0201,
+    CWB      = 0x0300,
+    EAPDPR   = 0x0502,
+    EAPIPR   = 0x0503,
     IIC      = 0x0750,
     IDC      = 0x0760,
     DWB      = 0x07A4,
     DTCMSize = 0x0910,
+    ITCMSize = 0x0911,
 };
 
 u32 CP15::get(u32 idx) {
@@ -42,6 +48,31 @@ void CP15::set(u32 idx, u32 data) {
 
             control = data;
             break;
+        case CP15Reg::CDPR:
+            std::printf("[ARM9:CP15 ] Write @ Cacheability (data protection region) = 0x%08X\n", data);
+            break;
+        case CP15Reg::CIPR:
+            std::printf("[ARM9:CP15 ] Write @ Cacheability (instruction protection region) = 0x%08X\n", data);
+            break;
+        case CP15Reg::CWB:
+            std::printf("[ARM9:CP15 ] Write @ Cache write bufferability = 0x%08X\n", data);
+            break;
+        case CP15Reg::EAPDPR:
+            std::printf("[ARM9:CP15 ] Write @ Extended access permission (data protection region) = 0x%08X\n", data);
+            break;
+        case CP15Reg::EAPIPR:
+            std::printf("[ARM9:CP15 ] Write @ Extended access permission (instruction protection region) = 0x%08X\n", data);
+            break;
+        case 0x0600:
+        case 0x0610:
+        case 0x0620:
+        case 0x0630:
+        case 0x0640:
+        case 0x0650:
+        case 0x0660:
+        case 0x0670:
+            std::printf("[ARM9:CP15 ] Write @ PU data region %u = 0x%08X\n", (idx >> 4) & 0xF, data);
+            break;
         case CP15Reg::IIC:
             std::printf("[ARM9:CP15 ] Invalidate instruction cache\n");
             break;
@@ -55,6 +86,11 @@ void CP15::set(u32 idx, u32 data) {
             std::printf("[ARM9:CP15 ] Write @ DTCM size = 0x%08X\n", data);
 
             dtcmSize = data & 0xFFFFF003E;
+            break;
+        case CP15Reg::ITCMSize:
+            std::printf("[ARM9:CP15 ] Write @ ITCM size = 0x%08X\n", data);
+
+            itcmSize = data & 0xFFFFF003E;
             break;
         default:
             std::printf("[ARM9:CP15 ] Unhandled write @ 0x%04X = 0x%08X\n", idx, data);
