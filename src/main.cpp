8a7e9fd8680874d8bf29bd3af65446b4f6b48a2b
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2023-04-24 22:27:04 +0200

    Fix argc stuff #2

--- src/main.cpp
+++ src/main.cpp
@@ -11,20 +11,20 @@
 int main(int argc, char **argv) {
     std::printf("[MariDS    ] Nintendo DS emulator\n");
 
-    if (argc < 3) {
+    if (argc < 4) {
         std::printf("Usage: MariDS /path/to/bios7 /path/to/bios9 /path/to/firm [/path/to/game] [-FASTBOOT]\n");
 
         return -1;
     }
 
     switch (argc) {
-        case 3:
+        case 4:
             nds::init(argv[1], argv[2], argv[3], NULL, false);
             break;
-        case 4: 
+        case 5: 
             nds::init(argv[1], argv[2], argv[3], argv[4], false);
             break;
-        case 5:
+        case 6:
         default:
             nds::init(argv[1], argv[2], argv[3], argv[4], std::strncmp(argv[5], "-FASTBOOT", 9) == 0);
             break;
