e76033503257b3a2df56234dad2eb8a73feb4e51
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2023-04-24 22:27:20 +0200

    Add ARM9 INTC regs

--- src/core/intc.cpp
+++ src/core/intc.cpp
@@ -47,7 +47,15 @@ void checkInterrupt7() {
     }
 }
 
-void sendInterrupt7(IntSource intSource) {
+void checkInterrupt9() {
+    if (ie9 & if9) {
+        unhaltCPU(9);
+
+        setIRQPending(9, ime9);
+    }
+}
+
+void sendInterrupt7(IntSource7 intSource) {
     std::printf("[INTC:ARM7 ] %s interrupt request\n", intNames[intSource]);
 
     if7 |= 1 << intSource;
@@ -68,6 +76,24 @@ u32 read32ARM7(u32 addr) {
             return if7;
         default:
             std::printf("[INTC:ARM7 ] Unhandled read32 @ 0x%08X\n", addr);
+
+            exit(0);
+    }
+}
+
+u32 read32ARM9(u32 addr) {
+    switch (addr) {
+        case INTCReg::IME:
+            std::printf("[INTC:ARM9 ] Read32 @ IME\n");
+            return ime9;
+        case INTCReg::IE:
+            std::printf("[INTC:ARM9 ] Read32 @ IE\n");
+            return ie9;
+        case INTCReg::IF:
+            std::printf("[INTC:ARM9 ] Read32 @ IF\n");
+            return if9;
+        default:
+            std::printf("[INTC:ARM9 ] Unhandled read32 @ 0x%08X\n", addr);
 
             exit(0);
     }
@@ -113,6 +139,48 @@ void write32ARM7(u32 addr, u32 data) {
     }
 
     checkInterrupt7();
+}
+
+void write8ARM9(u32 addr, u8 data) {
+    switch (addr) {
+        case INTCReg::IME:
+            std::printf("[INTC:ARM9 ] Write8 @ IME = 0x%02X\n", data);
+            
+            ime9 = data & 1;
+
+            checkInterrupt9();
+            break;
+        default:
+            std::printf("[INTC:ARM9 ] Unhandled write8 @ 0x%08X = 0x%02X\n", addr, data);
+
+            exit(0);
+    }
+}
+
+void write32ARM9(u32 addr, u32 data) {
+    switch (addr) {
+        case INTCReg::IME:
+            std::printf("[INTC:ARM9 ] Write32 @ IME = 0x%08X\n", data);
+            
+            ime9 = data & 1;
+            break;
+        case INTCReg::IE:
+            std::printf("[INTC:ARM9 ] Write32 @ IE = 0x%08X\n", data);
+            
+            ie9 = data;
+            break;
+        case INTCReg::IF:
+            std::printf("[INTC:ARM9 ] Write32 @ IF = 0x%08X\n", data);
+            
+            if9 &= ~data;
+            break;
+        default:
+            std::printf("[INTC:ARM9 ] Unhandled write32 @ 0x%08X = 0x%08X\n", addr, data);
+
+            exit(0);
+    }
+
+    checkInterrupt9();
 }
 
 }
--- src/core/intc.hpp
+++ src/core/intc.hpp
@@ -9,7 +9,7 @@
 
 namespace nds::intc {
 
-enum IntSource {
+enum IntSource7 {
     VBLANK, HBLANK, VCOUNT,
     Timer0, Timer1, Timer2, Timer3,
     RTC,
@@ -24,11 +24,16 @@ enum IntSource {
     WiFi,
 };
 
-void sendInterrupt7(IntSource intSource);
+void sendInterrupt7(IntSource7 intSource);
 
 u32 read32ARM7(u32 addr);
 
+u32 read32ARM9(u32 addr);
+
 void write8ARM7 (u32 addr, u8  data);
 void write32ARM7(u32 addr, u32 data);
+
+void write8ARM9 (u32 addr, u8  data);
+void write32ARM9(u32 addr, u32 data);
 
 }
