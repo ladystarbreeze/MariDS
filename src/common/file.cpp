d41e23be482bc75293c7d999b902ba0be8e59cc3
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2023-04-27 18:07:47 +0200

    Add binary writer

--- src/common/file.cpp
+++ src/common/file.cpp
@@ -15,3 +15,19 @@ std::vector<u8> loadBinary(const char *path) {
 
     return {std::istream_iterator<u8>{file}, {}};
 }
+
+void saveBinary(const char *path, u8 *data, size_t size) {
+    std::ofstream file;
+
+    file.open(path, std::ios::binary | std::ios::trunc);
+
+    if (!file.is_open()) {
+        std::printf("[MariDS    ] Unable to open file \"%s\"\n", path);
+
+        exit(0);
+    }
+
+    file.write((char *)data, size);
+
+    file.close();
+}
--- src/common/file.hpp
+++ src/common/file.hpp
@@ -11,3 +11,6 @@
 
 /* Reads a binary file into a std::vector */
 std::vector<u8> loadBinary(const char *path);
+
+/* Writes binary data into a file */
+void saveBinary(const char *path, u8 *data, size_t size);
